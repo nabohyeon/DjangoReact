d17f5249dd9abcd5d4ac543230449148c5777f9b
nabohyeon <nabb00@naver.com>
2022-10-25 22:33:34 +0900

    first commit

---
+++ mainApp/models.py
@@ -0,0 +1,11 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ mainApp/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+from .models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'title', 'content', 'updated_at')
